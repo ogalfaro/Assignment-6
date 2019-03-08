326286eba37bd8a5b9ac5ece5b480c84a82285b6
Oskar Alfaro <ogalfaro@ucsc.edu>
2019-03-07 20:13:50 -0800

    spawning system and animation

---
+++ Final_Assignment/Assets/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public Rigidbody Prefab;
+    public float time = 3f;
+
+    void Start(){
+        InvokeRepeating("SpawnAsteroid",time, time);
+    }
+
+    void SpawnAsteroid(){
+        Rigidbody RigidPrefab;
+        RigidPrefab = Instantiate(Prefab,Spawnpoint.position,Spawnpoint.rotation)as Rigidbody;
+    }
+}
