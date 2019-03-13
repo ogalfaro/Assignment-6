a844ce3388ca82b20ef28c48f9ee2d28b8c0204b
Oskar Alfaro <ogalfaro@ucsc.edu>
2019-03-12 22:18:50 -0700

    more stuff

---
+++ Final_Assignment/Assets/Teleport2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport2 : MonoBehaviour
+{
+    public Transform destination;
+    //public int points = 0;
+    
+void FixedUpdate() {
+    if(Input.GetKey(KeyCode.Z)) {
+        this.transform.position = new Vector3(-12.39f,4.02f,1.46f);//destination.position;
+    }
+    else if(Input.GetKey(KeyCode.X)) {
+        this.transform.position = new Vector3(-6.76f,4.02f,1.46f);//destination.position;
+    } 
+    else if(Input.GetKey(KeyCode.C)) {
+        this.transform.position = new Vector3(-.87f,4.02f,.51f);//destination.position;
+    }   
+}
+
+    
+}
